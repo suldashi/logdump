c193fc8056f8c6c50c8cfdac5e967ba37fd1abf2
Ermir Suldashi <ermirsuldashi@gmail.com>
2021-01-29 08:08:08 +0000

    added int route checker

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('db.sqlite');
 app.use(express.json());
 
-const port = 8080;
+const port = 8889;
 
 let head = `<!doctype html>
 <html>
@@ -23,14 +23,26 @@ let foot = `</ul<</body>
 app.use('/public', express.static(path.resolve(__dirname,"..",'public')));
 
 app.get("/",(req,res) => {
+	console.log("home");
     db.all("SELECT rowid AS id, log_tag, timestamp, log_content FROM logs ORDER BY timestamp DESC", function(err, rows) {
         let tags = rows.map(x => `<li><a href='/${x.id}'>${x.log_tag}: ${x.timestamp}</a></li>`).reduce((prev, curr) => prev + curr, "");
         res.send(head + tags + foot);
     });
 });
 
+function isInt(value) {
+  return !isNaN(value) && 
+         parseInt(Number(value)) == value && 
+         !isNaN(parseInt(value, 10));
+}
+
 app.get("/:id",(req,res) => {
     let rowId = req.params.id;
+	if(!isInt(rowId)) {
+	    res.status(404);
+	    res.send();
+	    return;
+	}
     db.get("SELECT log_content FROM logs WHERE rowid = ?", [rowId] , function(err, row) {
         let data = JSON.parse(row.log_content);
         data.info = data.info.map(JSON.parse);
@@ -40,6 +52,8 @@ app.get("/:id",(req,res) => {
         res.send(data);
     });
 });
+
+app.options("/submit", cors());
 
 app.post("/submit", cors(), (req,res) => {
     var stmt = db.prepare("INSERT INTO logs VALUES (?,?,?)");
